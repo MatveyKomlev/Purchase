1d410ed51362b1b4013dc03690b64b809d41f8b0
MatveyKomlev <komlev.mathew@gmail.com>
2025-04-09 05:48:18 +0300

    dev - Переписал Counter, использую инструменты blazorize, оставил Enum только для Статуса.

---
+++ Purchase/Data/ErpStatus.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Purchase.Data
+{
+    public enum ErpStatus : long
+    {
+        [Description("В процессе")]
+        InProgress = 0,
+
+        [Description("Отклонен")]
+        Rejected = 1,
+
+        [Description("Одобрен")]
+        Approved = 2
+    }
+
+    public static class ErpStatusExtensions
+    {
+        // Метод для получения локализованного имени статуса
+        public static string GetName(this ErpStatus status)
+        {
+            var field = typeof(ErpStatus).GetField(status.ToString());
+            if (field != null)
+            {
+                var attribute = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute;
+                if (attribute != null)
+                    return attribute.Description;
+            }
+
+            return status.ToString(); // Если описание не найдено, возвращаем строковое имя
+        }
+    }
+}
